d229c0be1eda818eb98a5a761982c58bb9949276
Nirvan Agrawal <ritesh.devices@gmail.com>
2026-02-15 13:14:19 +0530

    Removing redundant code and also adding hover for navbar

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-const featureItems = document.querySelectorAll('.feature-item');
-
-featureItems.forEach(item => {
-  item.addEventListener('click', () => {
-    featureItems.forEach(i => i.classList.remove('active'));
-    item.classList.add('active');
-  });
-});
-const radios = document.querySelectorAll('.slide-input');
-
-radios.forEach((radio, index) => {
-  radio.addEventListener('change', () => {
-    featureItems.forEach(i => i.classList.remove('active'));
-    featureItems[index].classList.add('active');
-  });
-});
 
 document.addEventListener("DOMContentLoaded", () => {
   const hamburger = document.querySelector(".hamburger");
